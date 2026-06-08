398a5b7e3e99c9df0a94ee92a27c568afffec361
Suganya841 <162861061+Suganya841@users.noreply.github.com>
2026-06-08 19:50:59 +0530

    Update expense.py

--- Backend/expense.py
+++ Backend/expense.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends
-from auth import get_current_user # Assuming auth.py provides this
+from Backend.auth import get_current_user # Assuming auth.py provides this
 
 router = APIRouter()
 
