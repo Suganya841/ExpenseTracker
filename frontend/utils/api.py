05745058745308c3652b9784415fe51316b45a61
Suganya841 <162861061+Suganya841@users.noreply.github.com>
2026-06-09 15:35:01 +0530

    Update api.py

--- frontend/utils/api.py
+++ frontend/utils/api.py
@@ -4,14 +4,18 @@ BASE_URL = "https://expensetracker-idh7.onrender.com"
 
 def login(username, password):
     try:
-        # data= use panrathu moolama data perfect ah URL-encoded form data ah backend ku pogum
         response = requests.post(
-            f"{BASE_URL}/login", 
-            data={"username": username, "password": password} 
+            f"{BASE_URL}/login",
+            data={
+                "username": username,
+                "password": password
+            }
         )
+
         if response.status_code == 200:
-            return response.json() 
+            return response.json()
         else:
-            return {"error": "Invalid credentials"}
-    except requests.exceptions.ConnectionError:
-        return {"error": "Backend server is not running"}
+            return {"error": response.text}
+
+    except Exception as e:
+        return {"error": str(e)}
